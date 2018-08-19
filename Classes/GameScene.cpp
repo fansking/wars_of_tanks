01c5a2e637da3e7f844bbf6865cb561a90391d68
jansona <alelipona121@gmail.com>
2018-08-19 17:38:34 +0800

    improve the controler II

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -110,26 +110,10 @@ void Game::onKeyPressed(EventKeyboard::KeyCode keyCode, Event * event)
 {
 	log("%d has been pressed", keyCode);
 	Vec2 playerPos = _player->getPosition();
-	switch ((int)keyCode)
-	{
-	case 146:
-		playerPos.y += _tileMap->getTileSize().height;
-		break;
-	case 142:
-		playerPos.y -= _tileMap->getTileSize().height;
-		break;
-	case 124:
-		playerPos.x -= _tileMap->getTileSize().width;
-		break;
-	case 127:
-		playerPos.x += _tileMap->getTileSize().width;
-		break;
-	}
-	this->setPlayerPosition(playerPos);
-
+	
 	mark = (int)keyCode;	//一个很丑陋的处理方式
 
-	this->schedule(schedule_selector(Game::keepMoving), 0.2f);
+	this->schedule(schedule_selector(Game::keepMoving), 0.1f);
 }
 
 void Game::onKeyReleased(EventKeyboard::KeyCode keyCode, Event * event)
