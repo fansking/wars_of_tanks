63570cd51d6b982505fcb819dbf7e0c1d86d7e7a
fansking <935802216@qq.com>
2018-08-23 09:48:20 +0800

    add coding standards

--- Classes/Bullet.h
+++ Classes/Bullet.h
@@ -13,8 +13,9 @@ class Bullet : public Sprite
 	CC_SYNTHESIZE(int, akt, AKT);
 
 public:
-	static TMXLayer   *walklay;
+	static TMXLayer   *_breakable0;
 	static TMXLayer   *coll;
+	static TMXLayer * _breakable1;
 	static Bullet * createWithImage();
 	void shootBulletFromTank(OurTank * tank);
 	virtual void update(float dt);
